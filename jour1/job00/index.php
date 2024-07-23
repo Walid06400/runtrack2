536a416e34a07b17e158b73cf43cf5f059d67479
walid <walid.rachdi@laplateforme.io>
2024-07-23 15:14:15 +0200

    job

--- jour1/job00/index.php
+++ jour1/job00/index.php
@@ -2,31 +2,11 @@
     $str = "laplateforme";
     $str2 = "vive ";
     $str3 = "!";
-    $str4 = $str2 . $str . $str3 ;
+    echo $str4 = $str2 . $str . $str3 ;
     $val = 6;
-    $val += 4;
+    $val += 5;
+    echo $val
     
 
-
-    
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<h1>Hello <?php=$str; ?></h1>
-<p>Bonjour <?=$str4;?></p>
-<p> <?php echo $str4; ?></p>
-<p> <?=$val;?></p>
-<p> <?=$val;?></p>
-
-
-
-</body>
-</html>
